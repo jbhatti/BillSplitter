ae82daec6e1b4496112a73497c67df06234f6c5f
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-16 20:40:41 -0400

    first commit

---
+++ BillSplitter2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter2
+//
+//  Created by Jaison Bhatti on 2017-09-16.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
